a4c985b86dce534b2bf21ec8008b921da03c3076
Karthik Parvathaneni <karthikparvathaneni2@gmail.com>
2024-07-02 23:50:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings"
+import ViewAll from "./components/ViewAll"
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAll />
+    </>
+  )
+}
+
+export default App
