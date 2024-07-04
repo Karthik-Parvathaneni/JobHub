0fa87235ec5fa81e426e9f17f958aa9b35a6fceb
Karthik Parvathaneni <karthikparvathaneni2@gmail.com>
2024-07-04 12:34:11 +0530

    updated pages

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line react/prop-types
-const Card = ({Children,bg='bg-gray-100'}) => {
+const Card = ({children,bg='bg-gray-100'}) => {
   return (
-    <div className={`${bg} p-6 rounded-lg shadow-md`}>{Children}</div>
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>
+        {children}</div>
   )
 }
 
